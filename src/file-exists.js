bbf69675b15cb713a761d685ab14c25f6f1700f7
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-23 15:12:58 +0200

    Fix early returns in function

--- src/file-exists.js
+++ src/file-exists.js
@@ -1,9 +1,10 @@
-const { readdirSync } = require('fs')
+const { readdirSync, existsSync } = require('fs')
 
 function fileExists(path, file) {
+  if (!existsSync(path)) return false
   const fileArray = readdirSync(path)
   const result = fileArray.filter(item => item == file)
-  if (result.includes(file)) return true
-  return false
+  if (!result.includes(file)) return false
+  return true
 }
 module.exports = fileExists
