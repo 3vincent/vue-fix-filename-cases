c22eefad8c4b1d26a91df4026ee629897dedbde8
Ben Sukstorf <bsukstorf@gmail.com>
2021-07-24 14:05:55 +0200

    Add await

--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ const { findRootFolder } = require('./start-up-check')
 const rewriteFilePaths = require('./rewrite-file-paths')
 
 async function rewriteVueFilesAndPaths() {
-  const vuejsRootFolder = findRootFolder()
+  const vuejsRootFolder = await findRootFolder()
+  console.log('nicht lustig: ', vuejsRootFolder)
   // eslint-disable-next-line no-restricted-syntax
   for await (const file of filesToRename) {
+    console.log('\n', '\x1b[42m', '\x1b[1m', '\x1b[37m', `> Handling: ${file.original.filename}`, '\x1b[0m')
     await renameFile(file.original.filename, `${vuejsRootFolder}${file.original.path}`)
     await rewriteFilePaths(
       file.original.filename,
       `${vuejsRootFolder}${file.linkedFrom.path}`,
       file.linkedFrom.filename
     )
-    console.log('')
   }
 }
 
